6e9f419e1f82ddcf5d03101771cc8ecd7e582a44
Lukas Schmidt <lukas.schmi@gmail.com>
2015-12-10 12:02:42 +0100

    Initial Commit

---
+++ ICEInformation/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ICEInformation
+//
+//  Created by Lukas Schmidt on 10.12.15.
+//  Copyright © 2015 Lukas Schmidt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
