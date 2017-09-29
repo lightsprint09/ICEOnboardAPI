8588c0d1f03d9dd24feb8ce13ac963c5ef960c83
Lukas Schmidt <lukas.la.schmidt@deutschebahn.com>
2017-09-29 13:24:41 +0200

    Adds wagon class to status

--- ICEInformationiOS/ICEStatus.swift
+++ ICEInformationiOS/ICEStatus.swift
@@ -8,19 +8,27 @@
 
 import Foundation
 
+public enum WagonClass: String, Codable {
+    case first = "FIRST"
+    case second = "SECOND"
+}
+
 public struct ICEStatus {
     public let location: Location
     public let speed: Float
+    public let wagonClass: WagonClass
 }
 
 extension ICEStatus: Decodable {
     enum CodingKeys: String, CodingKey {
         case speed = "speed"
+        case wagonClass
     }
     
     public init(from decoder: Decoder) throws {
         let container = try decoder.container(keyedBy: CodingKeys.self)
         speed = try container.decode(Float.self, forKey: .speed)
+        wagonClass = try container.decode(WagonClass.self, forKey: .wagonClass)
         location = try Location(from: decoder)
     }
 }
